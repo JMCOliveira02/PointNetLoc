e9db116689841e8ba84752d695a765eb43ff534b
JoaoOliveira <jmc_oliveira@hotmail.com>
2025-01-29 16:32:04 +0000

    Readme update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# PointNet++ for classification with single scale grouping 
+# Localization using the asbtraction layers of a pre-trained PointNet++ for classification with single scale grouping 
 * [link](https://github.com/yanx27/Pointnet_Pointnet2_pytorch/tree/master/log/classification/pointnet2_ssg_wo_normals)
--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import torch
 
 pt_cloud = o3d.io.read_point_cloud("C:/Users/jmc_o/Documents/A5/INESC_TEC/dev/Datasets/L_scenario/A/pcl/pcl0.pcd")
 xyz = np.asarray(pt_cloud.points)
+xyz = torch.from_numpy(xyz).float().unsqueeze(0).transpose(2, 1)
+print(xyz.shape)
 
 sa1 = PointNetSetAbstraction(npoint=512, radius=0.2, nsample=32, in_channel=3, mlp=[64, 64, 128], group_all=False)
-points, descriptors = 
+points, descriptors = sa1(xyz, None)
+
+
+print(points.shape)
+print(descriptors.shape)
