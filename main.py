8e4323cf386566a6abd3fb204075b8a895eb805b
JoaoOliveira <jmc_oliveira@hotmail.com>
2025-01-29 15:57:24 +0000

    x

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 from pointnet2_cls_ssg import get_model
+from pointnet2_utils import PointNetSetAbstraction
 import open3d as o3d
 import numpy as np
 import torch
 
 pt_cloud = o3d.io.read_point_cloud("C:/Users/jmc_o/Documents/A5/INESC_TEC/dev/Datasets/L_scenario/A/pcl/pcl0.pcd")
+xyz = np.asarray(pt_cloud.points)
 
-model = get_model(num_class=40, normal_channel=False)
-device = torch.device("cpu")
-model.to(device)
-model.eval()
-model = torch.load("best_model.pth", map_location=device, weights_only=False)
-
-points = np.array(pt_cloud.points)
-points -= np.mean(points, axis=0)
-points /= np.max(np.linalg.norm(points, axis=1))
-points_tensor = torch.from_numpy(points).float().unsqueeze(0).transpose(2, 1)
-
-# 4. Extract Features
-with torch.no_grad():  # No need to calculate gradients
-    model.eval()
-    features, critical_indexes, A_feat = model(points_tensor)
-
+sa1 = PointNetSetAbstraction(npoint=512, radius=0.2, nsample=32, in_channel=3, mlp=[64, 64, 128], group_all=False)
+points, descriptors = 
